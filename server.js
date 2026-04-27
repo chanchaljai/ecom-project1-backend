8081453341bdae8687d104b5ab6221f657d736e8
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-27 21:49:26 +0530

    backend deploy

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import authRoutes from './routes/authRoutes.js';
-import productRoutes from './routes/productsRoutes.js';
-import cartRoutes from './routes/cart.js';
-import addressRoutes from './routes/address.js';
-import orderRoutes from './routes/order.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productsRoutes.js";
+import cartRoutes from "./routes/cart.js";
+import addressRoutes from "./routes/address.js";
+import orderRoutes from "./routes/order.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors({
-  origin: "https://ecom-project1-frontend.vercel.app",
+  origin: [
+    "https://ecom-project1-frontend.vercel.app"
+  ],
   credentials: true
 }));
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/cart', cartRoutes);
-app.use('/api/address', addressRoutes);
-app.use('/api/order',orderRoutes )
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/address", addressRoutes);
+app.use("/api/order", orderRoutes);
 
-app.get('/', (req, res) => {
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 connectDB();
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
