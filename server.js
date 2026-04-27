70ee77c2666fb0fe4af6b93a693e0c438211a5a2
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-27 21:18:41 +0530

    backend deploy

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://ecom-project1-frontend.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
